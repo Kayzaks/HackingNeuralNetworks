70f81c5689e12614e59e3bd9f4e5c20a65599b76
Michael Kissner <m1o1d1@gmail.com>
2019-11-26 21:42:48 +0100

    Updated Solution for Exercise 3
    - The solution had a bug in it that made it report the wrong success rate. Fixed and modified it to work again, making the exercise slightly harder.
    - Adjusted the Exercise to include an easy version (the old one, which only works up to 5%) and a slightly harder one (the new 10%)

--- 3_BruteForcing/README.md
+++ 3_BruteForcing/README.md
@@ -2,8 +2,8 @@
 
 You are trying to Brute-Force a Image-based Security control. So far your attempt (see 'exercise.py') has proven to be unreliable. However, you have some vague idea of what an image that gives access should look like (see 'fake_id.png'), but it doesn't work either.
 
-- Develop a brute-force strategy that has a success rate of about 10% or better
+- Develop a brute-force strategy that has a success rate of about 5% or better (10% or better for a challenge)
 - Do not modify 'model.h5'
 - Do not simply draw a '4' in paint...
 
-A solution can be found in 'solution_3_0.py'
+A solution for 5% and 10% can be found in 'solution_3_0.py'
--- 3_BruteForcing/solution_3_0.py
+++ 3_BruteForcing/solution_3_0.py
@@ -1,9 +1,8 @@
 ''' 
-Solution to Exercise:
+Solution to Exercise 3:
 
 The idea is to add some noise to our best-guess fake-ID.
 '''
-
 
 
 import keras
@@ -16,10 +15,10 @@ model = keras.models.load_model('./model.h5')
 runs = 1000
 max_intensity = 10
 
-print('Running pure Noise Test')
+print('Running pure Random Test')
 successes = 0
 for i in range(runs):
-    # Creating a pure Noise Image
+    # Creating a pure Random Image
     processedImage = np.random.random([1, 28, 28, 1])
 
     # Run the Model and check what Digit was shown
@@ -29,10 +28,7 @@ for i in range(runs):
     if shownDigit == 4:
         successes = successes + 1
 
-print('Pure Noise had a ' + str(successes) + ' / ' + str(runs) + ' success rate')
-
-
-print('Running Best Guess + Noise Test')
+print('Pure Random had a ' + str(successes) + ' / ' + str(runs) + ' success rate')
 
 # Best-Guess Fake ID
 image = io.imread('./fake_id.png')
@@ -41,13 +37,17 @@ for yy in range(28):
     for xx in range(28):
         originalImage[0][xx][yy][0] = float(image[xx][yy]) / 255
 
-max_intensity = 10
 
-for intensity in range(max_intensity):
+print('Running Best Guess + Random Test')
+
+min_intensity = 40 # / 100
+max_intensity = 50 # / 100
+
+for intensity in range(min_intensity, max_intensity):
     successes = 0
     for i in range(runs):
-        # Adding some Noise to the image
-        noise = np.random.random([1, 28, 28, 1]) * float(intensity) * 0.1
+        # Adding some Random noise to the image
+        noise = np.random.random([1, 28, 28, 1]) * float(intensity) * 0.01
 
         processedImage = originalImage + noise
 
@@ -58,4 +58,28 @@ for intensity in range(max_intensity):
         if shownDigit == 4:
             successes = successes + 1
 
-    print('Intensity ' + str(intensity) + ' had a ' + str(successes) + ' / ' + str(runs) + ' success rate')
+    print('Rand-Intensity ' + str(intensity) + ' had a ' + str(successes) + ' / ' + str(runs) + ' success rate')
+
+
+
+print('Running Best Guess + Normal Distributed Noise Test')
+
+min_intensity = 25 # / 100
+max_intensity = 35 # / 100
+
+for mu in range(min_intensity, max_intensity):
+    successes = 0
+    for i in range(runs):
+        # Adding some Normal distributed Noise to the image
+        noise = np.random.normal(float(mu) * 0.01, 0.05, [1, 28, 28, 1]) 
+
+        processedImage = originalImage + noise
+
+        # Run the Model and check what Digit was shown
+        shownDigit = np.argmax(model.predict(processedImage))
+
+        # Only Digit 4 grants access!
+        if shownDigit == 4:
+            successes = successes + 1
+
+    print('Mu-intensity ' + str(mu) + ' had a ' + str(successes) + ' / ' + str(runs) + ' success rate')
